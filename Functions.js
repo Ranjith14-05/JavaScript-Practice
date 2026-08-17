62ff90df49ffc273507ecc1ad4dd2fe3cceec37c
Ranjith Kumar <ranjithc1405@gmail.com>
2026-08-17 07:50:12 +0530

    Updated Rock Paper Sisor with Function

--- Functions.js
+++ Functions.js
@@ -1,5 +1,17 @@
 // A function in JavaScript is a reusable block of code designed to perform a specific task. It can accept inputs through parameters and can return an output using the return statement.
 
+// Rules for naming a function in JavaScript:
+
+// 1. Start with a letter, _ or $
+// 2. Cannot start with a number
+// 3. Numbers are allowed after the first character
+// 4. No spaces are allowed
+// 5. Cannot use reserved keywords
+// 6. Function names are case-sensitive
+// 7. Use meaningful names
+// 8. Usually use camelCase
+
+
 function greet() {
     console.log("Hello");
 }
