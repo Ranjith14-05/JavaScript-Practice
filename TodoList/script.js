31e52d6a70a72b137ab2130023089a01656133c5
Ranjith Kumar <ranjithc1405@gmail.com>
2026-08-28 22:46:20 +0530

    Day06 : Updated TodoList App

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,18 +1,28 @@
 let todoList =[];
 
+
 function addtask(){
     let task = document.querySelector('.inp').value;
-    todoList.push(task);
-    console.log(todoList);
+    let date = document.querySelector('.date-inp').value;
+    todoList.push({
+        task,
+        date
+    });
     document.querySelector('.inp').value = '';
     renderTask();
 }
 
 function renderTask(){
     document.querySelector('.out').innerHTML = '';
-    
     for(let i =0 ; i < todoList.length; i++){
+        const task = todoList[i].task;
+        const date = todoList[i].date;
         document.querySelector('.out')
-            .innerHTML += `<p>${todoList[i]}</p>`;
+            .innerHTML += `<div>${task}</div>
+            <div>${date}</div>
+            <button onclick="todoList.splice(${i},1);
+            renderTask();" class="del">Delete</button>
+            `;
     }
 }
+let date = document.querySelector('.date-inp').value;
